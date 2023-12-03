ece4a8872ea53f1940784303957e4437ab7b5f76
benjaminsalon <benjaminsalon.business@gmail.com>
2023-12-03 23:49:00 +0100

    added nfts pallet

--- substrate-parachain-template/runtime/src/lib.rs
+++ substrate-parachain-template/runtime/src/lib.rs
@@ -20,6 +20,15 @@ use sp_runtime::{
 	ApplyExtrinsicResult, MultiSignature,
 };
 
+// For NFTs
+use pallet_nfts::PalletFeatures;
+pub type AssetsForceOrigin = EnsureRoot<AccountId>;
+
+use frame_support::traits::AsEnsureOriginWithArg;
+use frame_system::{ EnsureSigned};
+
+pub const UNITS: Balance = 1000000;
+
 use sp_std::prelude::*;
 #[cfg(feature = "std")]
 use sp_version::NativeVersion;
@@ -463,6 +472,45 @@ impl pallet_parachain_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+parameter_types! {
+	pub NftsPalletFeatures: PalletFeatures = PalletFeatures::all_enabled();
+	pub const NftsMaxDeadlineDuration: BlockNumber = 12 * 30 * DAYS;
+	pub const NftsCollectionDeposit: Balance = 10 * UNITS; // 10 UNIT deposit to create uniques class
+	pub const NftsItemDeposit: Balance = UNITS / 100; // 1 / 100 UNIT deposit to create uniques instance
+	pub const NftsMetadataDepositBase: Balance = 1;
+	pub const NftsAttributeDepositBase: Balance = 1;
+	pub const NftsDepositPerByte: Balance = 1;
+}
+
+impl pallet_nfts::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type CollectionId = u32;
+	type ItemId = u32;
+	type Currency = Balances;
+	type CreateOrigin = AsEnsureOriginWithArg<EnsureSigned<AccountId>>;
+	type ForceOrigin = AssetsForceOrigin;
+	type Locker = ();
+	type CollectionDeposit = NftsCollectionDeposit;
+	type ItemDeposit = NftsItemDeposit;
+	type MetadataDepositBase = NftsMetadataDepositBase;
+	type AttributeDepositBase = NftsAttributeDepositBase;
+	type DepositPerByte = NftsDepositPerByte;
+	type StringLimit = ConstU32<256>;
+	type KeyLimit = ConstU32<64>;
+	type ValueLimit = ConstU32<256>;
+	type ApprovalsLimit = ConstU32<20>;
+	type ItemAttributesApprovalsLimit = ConstU32<30>;
+	type MaxTips = ConstU32<10>;
+	type MaxDeadlineDuration = NftsMaxDeadlineDuration;
+	type MaxAttributesPerCall = ConstU32<10>;
+	type Features = NftsPalletFeatures;
+	type OffchainSignature = Signature;
+	type OffchainPublic = <Signature as Verify>::Signer;
+	type WeightInfo = weights::pallet_nfts::WeightInfo<Runtime>;
+	#[cfg(feature = "runtime-benchmarks")]
+	type Helper = ();
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime
@@ -495,6 +543,8 @@ construct_runtime!(
 
 		// Template
 		TemplatePallet: pallet_parachain_template = 50,
+
+		Nfts: pallet_nfts,
 	}
 );
 
